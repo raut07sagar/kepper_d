f9629861ff38d52d93cece9521a58a7e418c6a7f
Anuj Ambadas Giri <87684592+anujgiri20@users.noreply.github.com>
2021-12-23 17:52:55 +0700

    Update JWT.js

--- JWT.js
+++ JWT.js
@@ -1,34 +1,34 @@
 
-const jwt = require("jsonwebtoken");
-const verify = require("jsonwebtoken");
-const cors = require("cors");
-app.use(cors())
+// const jwt = require("jsonwebtoken");
+// const verify = require("jsonwebtoken");
+// const cors = require("cors");
+// app.use(cors())
 
-const createTokens = (user) => {
+// const createTokens = (user) => {
  
-  const accessToken = jwt.sign(
-    { id: user._id },
-    process.env.KEY,
-    {
-      expiresIn: "30d"
-    }
-  );
+//   const accessToken = jwt.sign(
+//     { id: user._id },
+//     process.env.KEY,
+//     {
+//       expiresIn: "30d"
+//     }
+//   );
 
-  return accessToken;
-};
+//   return accessToken;
+// };
 
-const validateToken = (req, res, next) => {
+// const validateToken = (req, res, next) => {
 
-  try {
-    const token = req.header("access-token");
-    if (!token) return res.status(403).send("Access denied.");
+//   try {
+//     const token = req.header("access-token");
+//     if (!token) return res.status(403).send("Access denied.");
 
-    const decoded = jwt.verify(token,process.env.KEY);
-    req.user = decoded;
-    next();
-} catch (error) {
-    res.status(400).send("Invalid token");
-}
-  };
+//     const decoded = jwt.verify(token,process.env.KEY);
+//     req.user = decoded;
+//     next();
+// } catch (error) {
+//     res.status(400).send("Invalid token");
+// }
+//   };
 
-  module.exports = { createTokens, validateToken };
+//   module.exports = { createTokens, validateToken };
