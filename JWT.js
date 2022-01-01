de256cdcf00524de68019f931dc050861c22d2a1
Anuj Ambadas Giri <87684592+anujgiri20@users.noreply.github.com>
2022-01-01 13:28:19 +0700

    Update JWT.js

--- JWT.js
+++ JWT.js
@@ -5,7 +5,7 @@ const verify = require("jsonwebtoken");
 const createTokens = async (user) => {
  
   const accessToken = await jwt.sign(
-    { id: user._id },
+    { id: user.username },
     "SECRET",
     {
       expiresIn: "2h"
